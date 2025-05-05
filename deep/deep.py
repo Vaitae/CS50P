dbe1d95f2a9d902204ba8af777846f1050b65e69
Vaishnavi <thv.bhat@gmail.com>
2025-05-05 17:10:33 +0530

    Added deep

---
+++ deep/deep.py
@@ -0,0 +1,6 @@
+ques=input("What is the answer to the Great Question of Life, the Universe and Everything?")
+ques=ques.strip().lower()
+if(ques=="42") or (ques=="forty-two") or (ques=="forty two"):
+    print("Yes")
+else:
+    print("No")
